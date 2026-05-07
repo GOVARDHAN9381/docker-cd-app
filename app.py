e8fb64dac23ba37b28a7060d5b04868b08436d78
GOVARDHAN9381 <192411137.simats@saveetha.com>
2026-05-07 06:05:50 +0530

    Initial commit

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Dockerized App Deployed Successfully!"
+    return "CI/CD Pipeline Working Successfully!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
